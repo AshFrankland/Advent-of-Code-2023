38e4651d69de8fca8b2f8d5e0ac595b109b436be
Ash Frankland <afrankland@spartaglobal.com>
2023-12-06 14:29:46 +0000

    Day 6 Solved

--- Day_06_puzzle.py
+++ Day_06_puzzle.py
@@ -1,17 +1,49 @@
-from pprint import pprint
+from math import sqrt, floor, ceil
 
 def get_input():
-    input = 'test_input.txt'
-    #input = 'Day_06_input.txt'
+    input = 'Day_06_input.txt'
     with open(input) as puzzle_input:
         txt = puzzle_input.readlines()
         raw_input = []
         for line in txt:
-            raw_input.append(line.rstrip('\n'))
-        return raw_input
+            raw_input.append(line.lstrip('TimeDistance: ').rstrip('\n'))
+        time_strs = raw_input[0].split(' ')
+        dist_strs = raw_input[1].split(' ')
+        while '' in time_strs:
+            time_strs.remove('')
+        while '' in dist_strs:
+            dist_strs.remove('')
+        times = []
+        dists = []
+        r_time_str = ''
+        r_dist_str = ''
+        for time in time_strs:
+            times.append(int(time))
+            r_time_str += time
+        for dist in dist_strs:
+            dists.append(int(dist))
+            r_dist_str += dist
+        return times, dists, [int(r_time_str)], [int(r_dist_str)]
+
+def get_margins(times, dists):
+    margins = []
+    for time in range(len(times)):
+        marg_low = (-times[time] + sqrt((times[time] * times[time]) - (4 * dists[time])))/-2
+        marg_hi = (-times[time] - sqrt((times[time] * times[time]) - (4 * dists[time])))/-2
+        margins.append([ceil(marg_low), floor(marg_hi)])
+        if (margins[time][0] * (times[time] - margins[time][0])) == dists[time]:
+            margins[time][0] += 1
+        if (margins[time][1] * (times[time] - margins[time][1])) == dists[time]:
+            margins[time][1] -= 1
+    count = 1
+    for margin in margins:
+        count *= margin[1] - margin[0] + 1
+    print(count)
 
 def main():
-    get_input()
+    times, dists, real_time, real_dist = get_input()
+    get_margins(times, dists)
+    get_margins(real_time, real_dist)
 
 if __name__ == '__main__':
     main()
