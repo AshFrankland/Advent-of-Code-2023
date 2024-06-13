6a585d1bbef94af2259ad585c40a4a3f8c506bdf
Ash Frankland <afrankland@spartaglobal.com>
2024-06-13 06:28:18 +0100

    Day 12 Solved!

--- Day_12_puzzle.py
+++ Day_12_puzzle.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-
 def get_input():
-    #input = 'test_input.txt'
     input = 'Day_12_input.txt'
     with open(input) as puzzle_input:
         txt = puzzle_input.readlines()
@@ -22,70 +19,62 @@ def get_input():
         return spr_strs, spr_keys
 
 def repair_checker(spr_str, spr_key):
-    active_strs = []
-    checked_strs = []
-    dots = []
-    hashes = []
+    spr_key = tuple(spr_key)
+    active_strs = {}
     for spr in spr_str:
-        if spr == '.' and active_strs == []:
-            pass
-        elif spr == '#' and active_strs == []:
-            active_strs = ['#']
-        elif spr == '?' and active_strs == []:
-            active_strs = ['.', '#']
+        new_strs = {}
+        if active_strs == {}:
+            if spr == '.':
+                active_strs[(0,)] = 1
+            elif spr == '#':
+                active_strs[(1,)] = 1
+            else:
+                active_strs[(0,)] = 1
+                active_strs[(1,)] = 1
         elif spr == '.':
-            active_strs = [active_str + '.' for active_str in active_strs]
             for active_str in active_strs:
-                if str_checker(active_str, spr_key):
-                    checked_strs.append(active_str)
-            active_strs = checked_strs
-            checked_strs = []
+                if active_str[-1] != 0:
+                    if active_str + (0,) in active_strs:
+                        active_strs[active_str + (0,)] = active_strs[active_str] + active_strs[active_str + (0,)]
+                    else:
+                        new_strs[active_str + (0,)] = active_strs[active_str]
+            for active_str in active_strs:
+                if active_str[-1] == 0:
+                    new_strs[active_str] = active_strs[active_str]
+            active_strs = new_strs
         elif spr == '#':
-            active_strs = [active_str + '#' for active_str in active_strs]
             for active_str in active_strs:
-                if str_checker(active_str, spr_key):
-                    checked_strs.append(active_str)
-            active_strs = checked_strs
-            checked_strs = []
-        elif spr == '?':
-            dots = [active_str + '.' for active_str in active_strs]
-            hashes = [active_str + '#' for active_str in active_strs]
-            active_strs = dots + hashes
-            dots = []
-            hashes = []
+                new_strs[active_str[:-1] + (active_str[-1] + 1,)] = active_strs[active_str]
+            active_strs = new_strs
+        else:
             for active_str in active_strs:
-                if str_checker(active_str, spr_key):
-                    checked_strs.append(active_str)
-            active_strs = checked_strs
-            checked_strs = []
-    valid_count = 0
-    for active_str in active_strs:
-        if final_check(active_str, spr_key):
-            valid_count += 1
-    return valid_count
-
-def str_checker(spr_str, spr_key):
-    spr_str = spr_str.lstrip('.')
-    break_count = 0
-    key_index = 0
-    spr_key.append(0)
-    for spr in spr_str:
-        if spr == '#':
-            break_count += 1
-            if break_count > spr_key[key_index]:
-                return False
-        elif spr == '.':
-            if break_count > 0:
-                if break_count != spr_key[key_index]:
-                    return False
-                key_index += 1
-                break_count = 0
-    return True
-
-def final_check(spr_str, spr_key):
-    if spr_str.count('#') == sum(spr_key):
-        return True
-    return False
+                new_strs[active_str[:-1] + (active_str[-1] + 1,)] = active_strs[active_str]
+            for active_str in active_strs:
+                if active_str[-1] != 0:
+                    if active_str + (0,) in active_strs:
+                        active_strs[active_str + (0,)] = active_strs[active_str] + active_strs[active_str + (0,)]
+                    else:
+                        new_strs[active_str + (0,)] = active_strs[active_str]
+            for active_str in active_strs:
+                if active_str[-1] == 0:
+                    new_strs[active_str] = active_strs[active_str]
+            active_strs = new_strs
+        dead_strs = []
+        for active_str in active_strs:
+            if len(active_str) > (len(spr_key) + 1):
+                dead_strs.append(active_str)
+            else:
+                for index in range(len(active_str) - 1):
+                    if active_str[index] != spr_key[index]:
+                        dead_strs.append(active_str)
+        for str in dead_strs:
+            active_strs.pop(str)
+    valid_strs = 0
+    if spr_key in active_strs:
+        valid_strs += active_strs[spr_key]
+    if (spr_key + (0,)) in active_strs:
+        valid_strs += active_strs[spr_key + (0,)]
+    return valid_strs
 
 def main():
     spr_strs, spr_keys = get_input()
@@ -93,6 +82,12 @@ def main():
     for index in range(len(spr_strs)):
         part1 += repair_checker(spr_strs[index], spr_keys[index])
     print(part1)
+    spr_strs = [spr_str + '?' + spr_str + '?' + spr_str + '?' + spr_str + '?' + spr_str for spr_str in spr_strs]
+    spr_keys = [spr_key + spr_key + spr_key + spr_key + spr_key for spr_key in spr_keys]
+    part2 = 0
+    for index in range(len(spr_strs)):
+        part2 += repair_checker(spr_strs[index], spr_keys[index])
+    print(part2)
 
 if __name__ == '__main__':
     main()
